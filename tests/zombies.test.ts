b3ed96abe807cdaab778b1bf4ac2f7d2983ef9a5
Amanda Olving <amanda.olving@gmail.com>
2025-09-11 11:41:47 +0200

    Add comments for clarification on what tests to implement

--- tests/zombies.test.ts
+++ tests/zombies.test.ts
@@ -27,4 +27,11 @@ test.skip("two-roomer is not full when a zombie is added", () => {});
 
 test.skip("second zombie consumes first zombie when added to a one-roomer", () => {});
 
-// You are free to add more tests that you think are relevant!
+// TODO: 
+// 1. Test to check if room is full 
+// 2. Test to check if empty room that fits one zombie is not full
+// 3. Test to consume first zombie if room is full (FIFO)
+// 4. Test to check that zombie is added to room if not full
+// 5. Test to check that room with capacity of 2 is not full when one zombie is added
+// 6. Test to check that room with capacity of 2 is full when two zombies are added
+// 7. Test to check that second zombie consumes first zombie when added to a one-roomer
